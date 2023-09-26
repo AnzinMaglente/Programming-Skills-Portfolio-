2f7a8482093cf017c3c8814b844b8177a2ee971d
Anzin-Maglente <144773742+AnzinMaglente@users.noreply.github.com>
2023-09-26 12:36:47 +0400

    Update Chapter 1 - Exercise 1.py

--- Chapter 1/Chapter 1 - Exercise 1.py
+++ Chapter 1/Chapter 1 - Exercise 1.py
@@ -14,5 +14,5 @@ Twinkle, twinkle, little star, How I wonder what you are! Up above the world so 
 
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high,\n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
 
-"""This prints out the poem 'Twinkle Twinkle Little Star' using a \t and \n command to add spacing 
+"""This prints out the poem 'Twinkle Twinkle Little Star' using a \t and \n command to add indentions 
 and make new lines to the sentences"""
