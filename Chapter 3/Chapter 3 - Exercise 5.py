d694a26167ff1db1ddea9f49194dc6b0c321cf75
Anzin-Maglente <144773742+AnzinMaglente@users.noreply.github.com>
2023-09-26 10:02:16 +0400

    Update Chapter 3 - Exercise 5.py

--- Chapter 3/Chapter 3 - Exercise 5.py
+++ Chapter 3/Chapter 3 - Exercise 5.py
@@ -15,7 +15,7 @@ thus, the list was changed.
 Invitees = ["Lianne", "Carl", "Elex", "Kent", "Aki"]
 for i in Invitees:
     print ("Dear " + i.title() + """.
-           You have been cordinally invited to my house for dinner, 
+           You have been cordially invited to my house for dinner, 
            please respond as soon as possible, thank you\n""")
 
 print("However, " + Invitees[0] + " is unable to come, so a new list was made\n")
@@ -23,5 +23,5 @@ del Invitees[0] #This deletes the first element in the list which would be "Lian
 Invitees.append("Fadi") #This adds an element at the end of the list
 for i in Invitees:
     print ("Dear " + i.title() + """,
-           You have been cordinally invited to my house for dinner, 
+           You have been cordially invited to my house for dinner, 
            please respond as soon as possible, thank you\n""")
